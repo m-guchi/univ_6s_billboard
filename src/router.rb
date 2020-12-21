a9bae466d0b60b008f1bc5e73ed87668e251bdaa
guchimina -desktop <kazu9sum.mi@gmail.com>
2020-12-21 14:53:57 +0900

    css routing

--- src/router.rb
+++ src/router.rb
@@ -14,40 +14,52 @@ class Router
     end
 
     def routing
+        @header = ""
+        @body = ""
         method = @method.method
-        if method == "GET" || method == "HEAD" || method == "POST"
+        if ["GET","HEAD","POST"].include?(method)
             if @paths.last.end_with?(".css")
-                return router_css(@paths)
+                router_css()
             else
                 case @paths[0]
                 when "index"
-                    status_code = 200
-                    header = "Content-Type: text/html; charset=utf-8"
+                    @status_code = 200
+                    @header = "Content-Type: text/html; charset=utf-8"
                     index_html = ERB.new(File.read(__dir__ + "/../templetes/index.html.erb"))
                     @csv = CSV.read(__dir__ + "/../data/text.csv")
-                    body = index_html.result(binding)
+                    @body = index_html.result(binding)
                 else
-                    status_code = 404
+                    @status_code = 404
                 end
             end
         else
-            status_code = 405
+            @status_code = 405
         end
-        return status_code, header, body
+        return @status_code, @header, @body
     end
 
-    # CSSファイルのルーティング
-    def router_css(paths)
-        path = paths.join("/")
-        file_path = __dir__ + "/../public/" + path
-        if File.exist?(file_path)
-            status_code = 200
-            header = "Content-Type: text/css; charset=utf-8"
-            body = ERB.new(File.read(file_path)).result(binding)
-        else
-            status_code = 404
+    def router_css
+        file_path = __dir__ + "/../public/" + @paths.join("/")
+        unless exist_file(file_path)
+            @status_code = 404
+            return 0
         end
-        return status_code, header, body
+        unless ["GET","HEAD"].include?(@method.method)
+            @status_code = 405
+            return 0
+        end
+        fetch_css_file(file_path)
     end
-    private:router_css
+
+    def exist_file(file_path)
+        return File.exist?(file_path)
+    end
+    private:exist_file
+
+    def fetch_css_file(file_path)
+        @status_code = 200
+        @header = "Content-Type: text/css; charset=utf-8"
+        @body = ERB.new(File.read(file_path)).result(binding)
+    end
+    private:fetch_css_file
 end
